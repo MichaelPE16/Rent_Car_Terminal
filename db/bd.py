71984be5b69381f831d928a3febc0ff7ec566d6a
MichaelPE16 <rafael112216@gmail.com>
2025-04-21 00:48:07 -0400

    database changes

--- db/bd.py
+++ db/bd.py
@@ -21,15 +21,70 @@ class DATABASE:
         return cursor.fetchall()
 
     # insert one
+    def insert(self, option):
+        
+        if option.lower() == 'one': 
+            table = input('Which table u want to use ->')
+
+            match table: 
+                case 'user':
+                    id_user = input('yout id ->') 
+                    name = input("Your name ->")
+                    lastname = input ('your lastname ->')
+                    wallet = input("yout wallet amount ->")
+
+                    with sqlite3.connect(self.path) as db: 
+                        cursor = db.cursor()
+                        cursor.execute(
+
+                            f"insert into {table} values (?,?,?,?)",
+                            (id_user, name, lastname, wallet),
+                        )
+
+
+
+                case 'vehicles': 
+
+                    id_vechicle = input('Vehicle id ->') 
+                    name = input("Vehicle name ->")
+                    model = input ('Vehicle model ->')
+                    year = input("Vehicle year ->")
+                    rent_price = input("Vehicle price")
+
+                    with sqlite3.connect(self.path) as db: 
+                        cursor = db.cursor()
+                        cursor.execute(
+
+                            f"insert into {table} values (?,?,?,?,?)",
+                            (id_vechicle, name, model,year, rent_price),
+                        )
+
+                case 'credentials': 
+                    pass
+                case 'car_rent': 
+                    pass 
+                case _: 
+                    raise sqlite3.OperationalError("Table does not exists!!")
+
+        
+
+
+
+        elif option.lower() == 'many': 
+            pass
+        else:
+            raise sqlite3.OperationalError("\n Inser method not available \n")
 
 
 
 
-if __name__ == "__main__": 
+#Test
+if __name__ == '__main__': 
     str_path = 'db/rentcar.db'
     database = DATABASE(str_path)
+    database.insert('oNe')
     table = input("Which table to read ->")
     data = database.read(table)
     print(data)
-
+    
     
